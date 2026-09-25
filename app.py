55be2d175d09883b5e7c3a7de42bbe592cef428c
rennym27 <rennym27@gmail.com>
2026-09-25 11:25:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ einnahmen_2026 = (n_erwachsene * b2026_em +
 # Betroffene Personen in Familientarifen
 fm_erwachsene = n_fm_1e
 fm_ehepaare = n_fm_2e * 2
-fm_vm = 0  # laut Matrix
+fm_vm = 5  # laut Matrix
 fm_jm = 8  # laut Matrix aus Excel Tabelle2
 fm_km = 14 # laut Matrix aus Excel Tabelle2
 
